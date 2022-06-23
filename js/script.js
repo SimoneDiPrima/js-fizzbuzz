009a1290353f5f9d81a606c12b3dc52831e92aae
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-23 17:06:10 +0200

    esercizio FizzBuzz terminato con stampa a video

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 
 const fullNumbers = document.getElementById("numbers");
+
 let boxNumber= document.getElementById("box-small");
 let content =document.getElementById("content");
-
 let singleNumber= "";
 
-
 for(let i = 1;i<=100;i++){
-     singleNumber += ` ${i}`;
-     console.log(singleNumber);
-    if(100 % singleNumber == 3){
-        let singleNumber = "fizz";
-        console.log(singleNumber)
+    singleNumber +=  `  ${i}`;
+    console.log(singleNumber);
+    if (i % 5 === 0 && i % 3 === 0){
+        singleNumber = singleNumber.replace(i , "BUZZFIZZ");
     }
-    else if(100 % singleNumber == 5){
-        let singleNumber = "buzz";
-        console.log(singleNumber);
+    if(i % 3 === 0){
+        singleNumber = singleNumber.replace(i , "FIZZ");  
     }
+    else if (i % 5 === 0){
+        singleNumber = singleNumber.replace(i , "BUZZ");
+    }
+    console.log(fullNumbers)
     fullNumbers.innerHTML = singleNumber;
 }
