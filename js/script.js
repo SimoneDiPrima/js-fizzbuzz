91afec2956b6e1e79f8a1592b7ba335cfe01a8c7
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-23 15:22:18 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+const fullNumbers = document.getElementById("numbers");
+let boxNumber= document.getElementById("box-small");
+let content =document.getElementById("content");
+
+let singleNumber= "";
+let fizzWord = "FIZZ" ;
+let buzzWord = "BUZZ";
+
+for(let i = 1;i<=100;i++){
+     singleNumber += ` ${i}`;
+     console.log(singleNumber);
+    if(100 % singleNumber == 3){
+        console.log(fizzWord);
+    }
+    else if(100 % singleNumber == 3){
+        console.log(buzzWord);
+    }
+}
+fullNumbers.innerHTML = singleNumber;
