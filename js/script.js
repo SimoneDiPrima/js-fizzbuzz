f477fe75a8ad150cd4c965b769a0b73b930fb646
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-23 15:31:30 +0200

    modifiche variabili

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ let boxNumber= document.getElementById("box-small");
 let content =document.getElementById("content");
 
 let singleNumber= "";
-let fizzWord = "FIZZ" ;
-let buzzWord = "BUZZ";
+
 
 for(let i = 1;i<=100;i++){
      singleNumber += ` ${i}`;
